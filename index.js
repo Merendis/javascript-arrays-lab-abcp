22436313ecb3a263b35023e7a281b17738ac6ad1
Meredith D Doucette <meredith.doucette@gmail.com>
2018-01-20 23:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = "I don't do much."
 var kittens = ["Buttons", "Frank"];
 
 destructivelyAppendKitten(name){
-  
+  kittens.push(name);
+  return kittens;
 }
+
+destructivelyAppendKitten("Milton");
